04b5ccced390c3fe2c00b082acf82cfa3814831c
TSCKANU <sunghyun7949@naver.com>
2020-08-10 16:13:56 +0900

    Initial Commit

---
+++ iOSCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCamera
+//
+//  Created by 임성현 on 2020/08/10.
+//  Copyright © 2020 임성현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
